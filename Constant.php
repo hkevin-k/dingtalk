757169f3a98f4af7a0651edd3d66b267aba7823b
hKevin <1946860243@qq.com>
2019-09-05 14:49:31 +0800

    add Constant

---
+++ Constant.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace DingTalk;
+
+/**
+ * Created by PhpStorm.
+ * User: zuodeng
+ * Date: 2018/7/18
+ * Time: 上午11:31
+ */
+class Constant
+{
+    static $CALL_TYPE_OAPI = "oapi";
+    static $CALL_TYPE_TOP = "top";
+
+    static $METHOD_POST = "POST";
+    static $METHOD_GET = "GET";
+
+    static $FORMAT_JSON = "json";
+    static $FORMAT_XML = "xml";
+}
